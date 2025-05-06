99bdb4e5160b11bd010dcdd867170001f2f798de
Владислава Лифар <vladislava.lufar@MacBook-Air.local>
2025-05-06 18:22:40 +0300

    Updates

---
+++ precache-manifest.eca84614761e80e7c547dc38e8fa78ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52bec9acaa7d64eee127a385ef254da",
+    "url": "/js_47-/index.html"
+  },
+  {
+    "revision": "38280705566f80cf24d1",
+    "url": "/js_47-/static/css/main.3a52514f.chunk.css"
+  },
+  {
+    "revision": "0f4acbfa16dc6bb0fb03",
+    "url": "/js_47-/static/js/2.8796042f.chunk.js"
+  },
+  {
+    "revision": "60c46b200b0cfd41617f",
+    "url": "/js_47-/static/js/3.b265e9c7.chunk.js"
+  },
+  {
+    "revision": "38280705566f80cf24d1",
+    "url": "/js_47-/static/js/main.31f3fed7.chunk.js"
+  },
+  {
+    "revision": "8b7d3f5e65456542bc93",
+    "url": "/js_47-/static/js/runtime~main.b16d7e92.js"
+  }
+]);
